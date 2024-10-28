1d39bfd509ec8e2df0cca3bfbc36a6c6420dfd85
Jonathan-Ultini <jonathanultini@gmail.com>
2024-10-28 14:13:01 +0100

    add controllers trainStation and tickets

---
+++ app/Http/Controllers/TrainStationController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\TrainStation;
+use Illuminate\Http\Request;
+
+class TrainStationController extends Controller
+{
+    public function index()
+    {
+        $stations = TrainStation::all();
+        return view('train-stations.index', compact('stations'));
+    }
+}
